e36eddc7c14df12575f69f64f9ab9e9edf132ae3
Lance Kluge <lance.kluge@gmail.com>
2025-03-29 16:41:23 -0700

    adds seemingly working db setup

---
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -0,0 +1,53 @@
+INSERT INTO users
+    (username, email, password)
+    VALUES
+    (
+        'test1', 'test1@gmail.com', 'wheeeeee'
+    ), 
+    (
+        'test2', 'test2@gmail.com', 'wheeeeee2222'
+    );
+
+
+INSERT INTO reviews 
+    (username, review, date_posted, rating)
+    VALUES 
+    (
+        'test1', 'Pretty good test run here at this mountain', '2022-07-22', 4.5
+    );
+
+INSERT INTO mountains
+    (mountain_name, location_name, latitude, longitude, pass, avg_rating, peak_elevation)
+    VALUES
+    (
+        'TestMT', 'Testing City', 31.11, 32.011, 'Epic', 3.5, 12000
+    );
+
+INSERT INTO images
+    (image_url, image_cap)
+    VALUES
+    (
+        'testing.com', 'Testing image'
+    );
+
+INSERT INTO mountains_to_reviews
+    (mountain_id, review_id)
+    VALUES
+    (
+        1, 1
+    );   
+
+INSERT INTO mountains_to_images
+    (mountain_id, image_id)
+    VALUES
+    (
+        0, 2
+    );   
+
+
+INSERT INTO reviews_to_images
+    (review_id, image_id)
+    VALUES
+    (
+        2, 4
+    );   
