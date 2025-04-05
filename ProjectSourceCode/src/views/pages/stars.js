7d97a95bfbc85e14decbaaebefe340191a7f25c2
Lance Kluge <lance.kluge@gmail.com>
2025-04-04 22:45:46 -0600

    adds difficulty, lift and snow cards

--- ProjectSourceCode/src/views/pages/stars.js
+++ ProjectSourceCode/src/views/pages/stars.js
@@ -1,8 +1,8 @@
 
 
 
-function setStarRating(rating) {
-    const starContainer = document.getElementById('star-rating');
+function setStarRating_xl(rating, star_Container) {
+    const starContainer = document.getElementById(star_Container);
     starContainer.innerHTML = '';
     for (let i = 0; i < Math.floor(rating); i++) {
         starContainer.innerHTML += '<i class="fa-solid fa-star fa-2xl" style="color: #FFD43B;"></i>';
@@ -12,5 +12,19 @@ function setStarRating(rating) {
     }
     for (let i = 0; i < 5 - Math.floor(rating) - (rating % 1 !== 0); i++) {
         starContainer.innerHTML += '<i class="fa-regular fa-star fa-2xl" style="color: #FFD43B;"></i>';
+    }
+}
+
+function setStarRating_large(rating, star_Container) {
+    const starContainer = document.getElementById(star_Container);
+    starContainer.innerHTML = '';
+    for (let i = 0; i < Math.floor(rating); i++) {
+        starContainer.innerHTML += '<i class="fa-solid fa-star fa-xl" style="color: #FFD43B;"></i>';
+    }
+    if (rating % 1 !== 0) {
+        starContainer.innerHTML += '<i class="fa-solid fa-star-half-stroke fa-xl" style="color: #FFD43B;"></i>';
+    }
+    for (let i = 0; i < 5 - Math.floor(rating) - (rating % 1 !== 0); i++) {
+        starContainer.innerHTML += '<i class="fa-regular fa-star fa-xl" style="color: #FFD43B;"></i>';
     }
 }
